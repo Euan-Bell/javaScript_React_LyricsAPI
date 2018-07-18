5743f37285e0cb9353d2fe763e213f6e53f9788d
Euan <bell_designs@outlook.com>
2018-07-18 11:27:13 +0100

    can render from SongContainer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import SongContainer from "./containers/SongContainer.js";
 
 
 class App extends Component {
   render() {
     return (
 
-      <div> placeholder </div>
+      <SongContainer />
     )
 
   }
---
+++ src/containers/SongContainer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Song from "../components/Song.js";
+
+
+
+class SongContainer extends React.Component{
+
+
+
+
+
+render(){
+  return(
+    <div>
+      Can reach songContainer
+    </div>
+  )
+}
+}
+
+export default SongContainer;
