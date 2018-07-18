929b2c0efc189ef32ef787cbc1cb08050387d2b3
Euan <bell_designs@outlook.com>
2018-07-18 12:02:17 +0100

    implemented api

--- src/containers/SongContainer.js
+++ src/containers/SongContainer.js
@@ -4,15 +4,31 @@ import Song from "../components/Song.js";
 
 
 class SongContainer extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      artist: "Adele",
+      title: "Hello",
+      lyrics: ""
+    }
+  }
 
+  componentDidMount(){
+    console.log('componentDidMount');
+    const url = 'https://api.lyrics.ovh/v1/adele/hello';
+    fetch(url)
+    .then(response => response.json())
+    .then(lyrics => this.setState({lyrics: lyrics}))
 
+    .catch(err => console.error(err));
+  }
 
 
 
 render(){
   return(
     <div>
-      Can reach songContainer
+      <h2>Select A Song!</h2>
     </div>
   )
 }
