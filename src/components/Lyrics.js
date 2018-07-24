4c9a86cb5de61303f2db59a33ca6ae1d77961fd9
Euan <bell_designs@outlook.com>
2018-07-24 13:18:29 +0100

    starting frontend

--- src/components/Lyrics.js
+++ src/components/Lyrics.js
@@ -3,8 +3,6 @@ import React from 'react';
 class Lyrics extends React.Component{
 
  render(){
-  // const altTag = `${this.props.title} by ${this.props.artist} `;
-
    return (<div className = "lyrics">
      <p>Artist: {this.props.artist}</p>
      <p>Title: {this.props.title}</p>
@@ -14,5 +12,4 @@ class Lyrics extends React.Component{
 
 
 }
-
 export default Lyrics;
